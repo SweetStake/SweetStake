55dede3202ba7994af4165d138c265b713e0b047
SweetStake <sweetstakedev@yahoo.com>
2015-09-24 19:59:12 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SweetStake
+SweetStake
+Ticker: SWEET
+Total coin supply: 1000000000
+100% Proof of Stake interest
+30 seconds block time
+Minimum staking age: 2 hours
+Maximum staking age: 256 days
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define sweetstake-qt message queue name
+#define BITCOINURI_QUEUE_NAME "SweetStakeURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
